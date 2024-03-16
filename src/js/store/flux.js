e3f4aea66409b323cb4a3c5c2dc6e10af732f41a
NilsonMarenco <137912131+NilsonMarenco@users.noreply.github.com>
2024-03-16 22:52:01 +0000

    home create

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,32 +12,48 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+
+			],
+			people: [],
+			planets: [],
+			vehicles: [],
+			favorites: []
+
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
 
-				//reset the global store
-				setStore({ demo: demo });
+			getPeople: () => {
+				fetch('https://www.swapi.tech/api/people')
+					.then((response) => response.json())
+					.then((datos) => {
+						console.log(datos)
+						setStore({ people: datos.results })
+					})
+			},
+
+			getPlanets: () => {
+				fetch('https://www.swapi.tech/api/planets')
+					.then((response) => response.json())
+					.then((datos) => {
+						console.log(datos)
+						setStore({ planets: datos.results })
+					})
+			},
+
+			getVehicles: () => {
+				fetch('https://www.swapi.tech/api/vehicles')
+					.then((response) => response.json())
+					.then((datos) => {
+						console.log(datos)
+						setStore({ vehicles: datos.results })
+					})
+			},
+			addToFavorites: () => {
+
 			}
+
+
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,61 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import '../../styles/home.css'
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store } = useContext(Context);
+
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					<h2 className="item-name">Characters</h2>
+					<div className="d-flex flex-row container-tarjetas" style={{ overflowX: "auto" }}>
+						{store.people.map((character) => (
+							<div className="m-3" key={character.uid}>
+								<img src={`https://starwars-visualguide.com/assets/img/characters/${character.uid}.jpg`} />
+								<h3 className="name">{character.name}</h3>
+								<button type="button" class="btn btn-outline-warning">
+									<span className="heart-icon">♥</span>
+								</button>
+
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col">
+					<h2 className="item-name">Vehicles</h2>
+					<div className="d-flex flex-row container-tarjetas" style={{ overflowX: "auto" }}>
+						{store.vehicles.map((vehicle) => (
+							<div className="m-3" key={vehicle.uid}>
+								<img src={`https://starwars-visualguide.com/assets/img/vehicles/${vehicle.uid}.jpg`} />
+								<h4 className="name">{vehicle.name}</h4>
+								<button type="button" class="btn btn-outline-warning">
+									<span className="heart-icon">♥</span>
+								</button>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col">
+					<h2 className="item-name">Planets</h2>
+					<div className="d-flex flex-row container-tarjetas" style={{ overflowX: "auto" }}>
+						{store.planets.map((planet) => (
+							<div className="m-3" key={planet.uid}>
+								<img src={`https://starwars-visualguide.com/assets/img/planets/${planet.uid}.jpg`} />
+								<h3 className="name">{planet.name}</h3>
+								<button type="button" class="btn btn-outline-warning">
+									<span className="heart-icon">♥</span>
+								</button>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
